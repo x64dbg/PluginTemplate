d24cbf6a14514c053643a4dde8e90ac68fbac9d1
mrexodia <mr.exodia.tpodt@gmail.com>
2017-09-02 18:40:54 +0200

    added dprintf and dputs helpers

--- PluginTemplate/pluginmain.h
+++ PluginTemplate/pluginmain.h
@@ -52,6 +52,8 @@
 
 #define Cmd(x) DbgCmdExecDirect(x)
 #define Eval(x) DbgValFromString(x)
+#define dprintf(x, ...) _plugin_logprintf("[" PLUGIN_NAME "] " x, __VA_ARGS__)
+#define dputs(x) _plugin_logputs("[" PLUGIN_NAME "] " x)
 #define PLUG_EXPORT extern "C" __declspec(dllexport)
 
 //superglobal variables
