5b79a8af47efc5df6f51c65c8610cd29c28b2b1f
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2020-11-06 23:42:03 +0100

    Initial commit

---
+++ plugin.cpp
@@ -0,0 +1,17 @@
+#include "plugin.h"
+
+//Initialize your plugin data here.
+bool pluginInit(PLUG_INITSTRUCT* initStruct)
+{
+    return true; //Return false to cancel loading the plugin.
+}
+
+//Deinitialize your plugin data here.
+void pluginStop()
+{
+}
+
+//Do GUI/Menu related things here.
+void pluginSetup()
+{
+}
